1cd5a6a9699165614cf748c8ca611630f6c11972
programmeroscar <programmeroscar@gmail.com>
2020-04-15 08:36:15 -0700

    updated api.php

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
+
 Route::apiResource('post', 'PostController');
 Route::apiResource('region', 'RegionController');
+Route::apiResource('town', 'TownController');
+Route::apiResource('equipment', 'EquipmentController');
+Route::apiResource('customer','CustomerController');
+Route::get('coviddata', 'ConvidDataController@getCovidData');
