c34eadb2db99a305484708fe8d302f710dbf1a0b
Avalon <hopegag@gmail.com>
2020-04-15 10:58:43 +0000

    Added age to Customer Model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends ApiModel
 {
     protected $table = 'customers';
-    protected $fillable = ['firstname', 'lastname', 'location'];
+    protected $fillable = ['firstname', 'lastname', 'location', 'age'];
 }
