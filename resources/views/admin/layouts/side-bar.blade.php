bb5256631740cc2b37d7b22838c9c6fd568c5956
programmeroscar <programmeroscar@gmail.com>
2020-04-17 16:30:04 -0700

    created town form and added to side bar

--- resources/views/admin/layouts/side-bar.blade.php
+++ resources/views/admin/layouts/side-bar.blade.php
@@ -170,6 +170,13 @@
                                         <li><a href="{{route('region.index')}}">Region List</a></li>
                                         <li><a href="#">Update Region</a></li>
                                     </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#"><i class="dripicons-view-list-large"></i><span class="w-100">Town</span><span class="menu-arrow"><i class="mdi mdi-chevron-right"></i></span></a>
+                                    <ul class="nav-second-level" aria-expanded="false">
+                                        <li><a href="{{route('town.index')}}">Town List</a></li>
+                                        <li><a href="#">Town Region</a></li>
+                                    </ul>
                                 </li><!--end nav-item-->
                             </ul><!--end nav-->
                         </div><!-- end Others -->
--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,6 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Web'], function() {
     Route::get('dashboard', 'DashboardController@loadDashboard')->name('dashboard.index');
     Route::resource('post', 'PostController');
     Route::resource('region', 'RegionController');
+    Route::resource('town', 'TownController');
 });
 
