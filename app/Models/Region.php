c4020d5dbb732751046bfac6b864627ccce49280
programmeroscar <programmeroscar@gmail.com>
2020-04-17 09:50:24 -0700

    Deleted created and updated at filed from region

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Region extends ApiModel
 {
     protected $table = 'region';
-    protected $fillable = ['name', 'status', 'created_at', 'updated_at'];
+    protected $fillable = ['name', 'status'];
 }
