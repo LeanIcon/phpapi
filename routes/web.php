8ffaf356955f5362465511e7671cba4b9706ad94
Avalon <hopegag@gmail.com>
2020-04-22 06:42:19 +0000

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -37,4 +37,4 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Web'], function() {
 
 // Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
