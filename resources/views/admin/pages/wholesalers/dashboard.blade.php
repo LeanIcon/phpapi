c7593328b59b08eb88a9ca35ce73cb7d45493bb1
programmeroscar <programmeroscar@gmail.com>
2020-05-02 00:30:39 +0000

    UPDATED wholesaler dashboard

--- resources/views/admin/pages/wholesalers/dashboard.blade.php
+++ resources/views/admin/pages/wholesalers/dashboard.blade.php
@@ -6,10 +6,10 @@
     <div class="col-lg-3">
         <div class="card card-eco">
             <div class="card-body">
-                <h4 class="title-text mt-0">Total Products</h4>
+                <a href="#">  <h4 class="title-text mt-0">Total Products</h4></a>
                 <div class="d-flex justify-content-between">
                     <h3 class="text-purple">24k</h3>
-                    <i class="dripicons-user-group card-eco-icon bg-icon-purple align-self-center"></i>
+                    <a href="#">    <i class="dripicons-user-group card-eco-icon bg-icon-purple align-self-center"></i></a>
                 </div>
                 <p class="mb-0 text-muted text-truncate"><span class="text-success"><i class="mdi mdi-trending-up"></i>8.5%</span>Up From Yesterday</p>
             </div><!--end card-body-->
@@ -18,10 +18,10 @@
     <div class="col-lg-3">
         <div class="card card-eco">
             <div class="card-body">
-                <h4 class="title-text mt-0">Purchase Orders</h4>
+                <a href="#"> <h4 class="title-text mt-0">Purchase Orders</h4></a>
                 <div class="d-flex justify-content-between">
                     <h3 class="text-pink">10k</h3>
-                    <i class="dripicons-cart card-eco-icon bg-icon-pink align-self-center"></i>
+                    <a href="#">  <i class="dripicons-cart card-eco-icon bg-icon-pink align-self-center"></i></a>
                 </div>
                 <p class="mb-0 text-muted text-truncate"><span class="text-success"><i class="mdi mdi-trending-up"></i>1.5%</span> Up From Last Week</p>
             </div><!--end card-body-->
@@ -30,10 +30,10 @@
     <div class="col-lg-3">
         <div class="card card-eco">
             <div class="card-body">
-                <h4 class="title-text mt-0">Invoice</h4>
+                <a href="#">   <h4 class="title-text mt-0">Invoice</h4></a>
                 <div class="d-flex justify-content-between">
                     <h3 class="text-secondary">8400</h3>
-                    <i class="dripicons-jewel card-eco-icon bg-icon-secondary align-self-center"></i>
+                    <a href="#">    <i class="dripicons-jewel card-eco-icon bg-icon-secondary align-self-center"></i></a>
                 </div>
                 <p class="mb-0 text-muted text-truncate"><span class="text-danger"><i class="mdi mdi-trending-down"></i>3%</span> Down From Last Month</p>
             </div><!--end card-body-->
@@ -42,10 +42,10 @@
     <div class="col-lg-3">
         <div class="card card-eco">
             <div class="card-body">
-                <h4 class="title-text mt-0">Pending</h4>
+                <a href="#">  <h4 class="title-text mt-0">Pending</h4></a>
                 <div class="d-flex justify-content-between">
                     <h3 class="text-warning">$1590</h3>
-                    <i class="dripicons-wallet card-eco-icon bg-icon-warning  align-self-center"></i>
+                    <a href="#">  <i class="dripicons-wallet card-eco-icon bg-icon-warning  align-self-center"></i></a>
                 </div>
                 <p class="mb-0 text-muted text-truncate"><span class="text-success"><i class="mdi mdi-trending-up"></i>10.5%</span> Up From Yesterday</p>
             </div><!--end card-body-->
