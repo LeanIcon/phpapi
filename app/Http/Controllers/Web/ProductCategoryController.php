5de14c2141dde60c721ea94a1e3fe64e23444b4b
Avalon <hopegag@gmail.com>
2020-05-04 14:15:18 +0000

    updated

--- app/Http/Controllers/Web/ProductCategoryController.php
+++ app/Http/Controllers/Web/ProductCategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Web;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Models\ProductCategory;
 use App\Http\Controllers\Controller;
@@ -44,6 +45,7 @@ class ProductCategoryController extends Controller
      */
     public function store(Request $request)
     {
+        $request['slug'] = Str::slug($request->name);
         $productCategory = $this->productCategory::create($request->all());
         return redirect()->route('product_category.index');
     }
