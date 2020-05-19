e960ac2002d51852caf72cc5db454c1a899b99d9
gaby96 <gabrieladdo64@gmail.com>
2020-05-19 21:09:55 +0000

    wholesalerpurchaseorderreceivedcard

--- resources/views/admin/pages/wholesalers/purchaseorderlist.blade.php
+++ resources/views/admin/pages/wholesalers/purchaseorderlist.blade.php
@@ -27,168 +27,50 @@
                             <label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="datatable"></label>
                         </div>
                     </div>
-                </div>                <div class="table-responsive">
-                    <table class="table table-hover mb-0">
-                        <thead class="thead-light">
-                            <tr>
-                                <th class="border-top-0">PO Id</th>
-                                <th class="border-top-0">Retailer</th>
-                                <th class="border-top-0">Location</th>
-                                <th class="border-top-0">Order Date/Time</th>
-                                <th class="border-top-0">Qty</th>                                    
-                                <th class="border-top-0">Amount (₵)</th>
-                                <th class="border-top-0">Status</th>
-                            </tr><!--end tr-->
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <a href="{{route('wholesaler.purchaseorder')}}" class="d-inline-block align-middle mb-0 product-name">001</a>                                 </td>
-                                <td>
-                                    <a href="{{route('wholesaler.purchaseorder')}}" class="d-inline-block align-middle mb-0 product-name">Darko Oscar</a> 
-                                </td>
-                                <td>   
-                                   Accra                                                            
-                                </td>
-                                <td>3/03/2019 4:29 PM</td>
-                                <td>200</td>                                   
-                                <td> ₵750</td>
-                                <td>                                                                        
-                                    <span class="badge badge-md badge-boxed  badge-soft-success">Shipped</span>
-                                </td>
-                            </tr><!--end tr-->
-                            <tr>
-                                <td>
-                                    <a href="{{route('wholesaler.purchaseorder')}}" class="d-inline-block align-middle mb-0 product-name">002</a>                                 </td>
+                </div>                <div class="row">
+                        <div class="col-sm-12">
+                            <table id="datatable" class="table table-bordered dt-responsive nowrap dataTable no-footer" style="border-collapse: collapse; border-spacing: 0px; width: 100%;" role="grid" aria-describedby="datatable_info">
+                                <thead>
+                                    <tr role="row">
+                                        <th class="sorting_asc" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Product Name: activate to sort column descending" style="width: 285px;"> Purchase ID</th>
+                                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Category: activate to sort column ascending" style="width: 110px;">Reatailer Name</th>
+                                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Price: activate to sort column ascending" style="width: 69px;">Status</th>
+                                        <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Price: activate to sort column ascending" style="width: 69px;">View Purchase Order Details</th>
+                                        
+                                        
+                                        
+                                    </tr>
+                                </thead>
 
-                                </td>
-                                <td>
-                                    <a href="{{route('wholesaler.purchaseorder')}}" class="d-inline-block align-middle mb-0 product-name">Darko Oscar</a> 
+                                <tbody>
 
-                                </td>
-                                <td>   
-                                    Accra                                                            
-                                </td>
-                                <td>3/03/2019 4:29 PM</td>
-                                <td>200</td>                                   
-                                <td> ₵750</td>
-                                <td>                                                                        
-                                    <span class="badge badge-md badge-boxed  badge-soft-danger">Delivered</span>
-                                </td>
-                            </tr><!--end tr--> <tr>
-                                <td>
-                                    <a href="{{route('wholesaler.purchaseorder')}}" class="d-inline-block align-middle mb-0 product-name">003</a>                                 </td>
+                                    @if ($purchaseOrders->isNotEmpty())
+                                        @foreach ($purchaseOrders as $purchaseOrder)
+                                            <tr role="row" class="odd">
+                                                <td class="sorting_1">
+                                                    {{-- <img src="../assets/images/products/img-2.png" alt="" height="52"> --}}
+                                                    <p class="d-inline-block align-middle mb-0">
+                                                        <a href="{{route('wholesaler.order_details', $purchaseOrder->id)}}">
+                                                        <span class="badge badge-soft">PO-00{{$purchaseOrder->id}} </span>
+                                                        </a>
+                                                    </p>
+                                                </td>
+                                                <td>{{$purchaseOrder->retailer->name}}</td>
+                                                <td><span class="badge badge-soft-warning">{{$purchaseOrder->status}}</span></td>
+                                                <td>
+                                                    <a href="{{route('wholesaler.order_details', $purchaseOrder->id)}}"><i class="far fa-eye text-danger"></i></a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    @endif
 
-                                </td>
-                                <td>
-                                    <a href="{{route('wholesaler.purchaseorder')}}" class="d-inline-block align-middle mb-0 product-name">Darko Oscar</a> 
-                                </td>
-                                <td>   
-                                    Accra                                                            
-                                </td>
-                                <td>3/03/2019 4:29 PM</td>
-                                <td>200</td>                                   
-                                <td> ₵750</td>
-                                <td>                                                                        
-                                    <span class="badge badge-md badge-boxed badge-soft-warning">Pending</span>
-                                </td>
-                            </tr><!--end tr--> <tr>
-                                <td>
-                                    <a href="{{route('wholesaler.purchaseorder')}}" class="d-inline-block align-middle mb-0 product-name">004</a>                                 </td>
-
-                                </td>
-                                <td>
-                                    <a href="{{route('wholesaler.purchaseorder')}}" class="d-inline-block align-middle mb-0 product-name">Darko Oscar</a> 
-                                </td>
-                                <td>   
-                                    Accra                                                            
-                                </td>
-                                <td>3/03/2019 4:29 PM</td>
-                                <td>200</td>                                   
-                                <td> ₵750</td>
-                                <td>                                                                        
-                                    <span class="badge badge-md badge-boxed  badge-soft-success">Shipped</span>
-                                </td>
-                            </tr><!--end tr-->
-                                                                              
-                        </tbody>
-                    </table> <!--end table-->                                               
-                </div><!--end /div-->
-            </div><!--end card-body-->
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
         </div><!--end card-->
     </div><!--end col-->
 </div><!--end row-->
 <!--New Table start-->
-
-<!--Map Dashboard-->
-<!--<div class="row">
-    <div class="col-lg-8">
-        <div class="card">
-            <div class="card-body">
-                <div class="float-right eco-revene-history justify-content-end">
-                    <ul class="nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Today</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Yesterday</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Last Week</a>
-                        </li>                                     
-                    </ul>
-                </div>
-                <h4 class="header-title mt-0">Revenue</h4>
-                <div class="apexchart-wrapper">
-                    <div id="eco-dash1" class="chart-gutters"></div>
-                </div>
-            </div>--><!--end card-body-->
-        </div><!--end card-->
-    </div><!--end col-->
-    <!--<div class="col-lg-4">
-        <div class="card">
-            <div class="card-body">
-                <h4 class="header-title mt-0">Top Globle Sales</h4>
-                <div id="world-map-markers" class="dashboard-map drop-shadow-map"></div>
-                <div class="row">
-                    <div class="col-md-5">
-                        <div class="mt-3">
-                            <span class="text-info">USA</span>
-                            <small class="float-right text-muted ml-3 font-14">81%</small>
-                            <div class="progress mt-2" style="height:3px;">
-                                <div class="progress-bar bg-pink" role="progressbar" style="width: 81%; border-radius:5px;" aria-valuenow="81" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-
-                        <div class="mt-3">
-                            <span class="text-info">Greenland</span>
-                            <small class="float-right text-muted ml-3 font-14">68%</small>
-                            <div class="progress mt-2" style="height:3px;">
-                                <div class="progress-bar bg-secondary" role="progressbar" style="width: 68%; border-radius:5px;" aria-valuenow="68" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>--><!--end col-->
-               <!--     <div class="col-md-5 ml-auto">                                            
-                        <div class="mt-3">
-                            <span class="text-info">Australia</span>
-                            <small class="float-right text-muted ml-3 font-14">48%</small>
-                            <div class="progress mt-2" style="height:3px;">
-                                <div class="progress-bar bg-purple" role="progressbar" style="width: 48%; border-radius:5px;" aria-valuenow="48" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                        
-                        <div class="mt-3">
-                            <span class="text-info">Brazil</span>
-                            <small class="float-right text-muted ml-3 font-14">32%</small>
-                            <div class="progress mt-2" style="height:3px;">
-                                <div class="progress-bar bg-warning" role="progressbar" style="width: 32%; border-radius:5px;" aria-valuenow="32" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>--><!--end col-->
-                </div><!--end row-->
-            </div><!--end card-body-->
-        </div><!--end card-->
-    </div><!--end col-->
-</div><!--end row-->
 
 @endsection
