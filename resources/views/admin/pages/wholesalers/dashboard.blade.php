507a2e6f33efed956a1c35ddeced57ffa862ecfc
programmeroscar <programmeroscar@gmail.com>
2020-05-21 12:46:51 +0000

    added greetings to wholesaler dashboard

--- resources/views/admin/pages/wholesalers/dashboard.blade.php
+++ resources/views/admin/pages/wholesalers/dashboard.blade.php
@@ -8,7 +8,28 @@
 
 <div class="row">
 <div class="col-lg-6 tags p-b-2">
-                    <h3> Hi {{ $wholesalername }}, </h3>
+    <?php
+    /* This sets the $time variable to the current hour in the 24 hour clock format */
+    $time = date("H");
+    /* Set the $timezone variable to become the current timezone */
+    $timezone = date("e");
+    /* If the time is less than 1200 hours, show good morning */
+    if ($time < "12") {
+        echo "<h3>".  "Good morning" . $wholesalername.","."</h3>";
+    } else
+    /* If the time is grater than or equal to 1200 hours, but less than 1700 hours, so good afternoon */
+    if ($time >= "12" && $time < "17") {
+        echo "<h3>". "Good afternoon" . " " . $wholesalername .","."</h3>";
+    } else
+    /* Should the time be between or equal to 1700 and 1900 hours, show good evening */
+    if ($time >= "17" && $time < "19") {
+        echo "Good evening";
+    } else
+    /* Finally, show good night if the time is greater than or equal to 1900 hours */
+    if ($time >= "19") {
+        echo "<h3>". "Good night" . $wholesalername.","."</h3>";
+    }
+    ?>
                     <h1> Welcome Back! </h1>
                     
                 </div>
