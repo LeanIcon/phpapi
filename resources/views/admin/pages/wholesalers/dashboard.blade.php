af06133066e7d554b234b9710521ffcce336739d
Avalon <hopegag@gmail.com>
2020-05-21 22:49:23 +0000

    Updated Name Display

--- resources/views/admin/pages/wholesalers/dashboard.blade.php
+++ resources/views/admin/pages/wholesalers/dashboard.blade.php
@@ -8,7 +8,7 @@
 
 <div class="row">
 <div class="col-lg-6 tags p-b-2">
-                    <h3> Hi, </h3>
+                    <h3> Hi {{Auth::user()->name}} </h3>
                     <h1> Welcome Back! </h1>
                     
                 </div>
