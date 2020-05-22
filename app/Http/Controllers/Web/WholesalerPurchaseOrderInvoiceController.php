81ffeaa5d45ba279340b1beaead2fa23e398d60d
Avalon <hopegag@gmail.com>
2020-05-22 05:55:01 +0000

    added wholesaler invoices

--- app/Http/Controllers/Web/WholesalerPurchaseOrderInvoiceController.php
+++ app/Http/Controllers/Web/WholesalerPurchaseOrderInvoiceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Models\PurchaseOrders;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class WholesalerPurchaseOrderInvoiceController extends Controller
 {
@@ -20,10 +21,12 @@ class WholesalerPurchaseOrderInvoiceController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($purchaseId = null)
     {
+        $user = Auth::user();
+        $purchaseInvoices = $user->wholesaler_orders->where('invoice', '!=', '');
         $pageTitle = 'Purchase Order Invoice';
-        return view('admin.pages.wholesalers.purchaseorderinvoice');
+        return view('admin.pages.wholesalers.purchaseorderinvoice', compact('purchaseInvoices'));
     }
 
     public function invoicedetail()
