a7897e26f5a377fcbe872c74c613c8d7c1ba7d93
gaby96 <gabrieladdo64@gmail.com>
2020-06-05 00:38:36 +0000

    added remember me

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,14 +12,14 @@
   <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="admin/assets/css/login.css">
- <main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
+ <main class="d-flex align-items-right min-vh-100 py-3 py-md-0">
     <div class="container">
       <div class="card login-card">
         <div class="row no-gutters">
           <div class="col-md-5">
             <img src="admin/assets/images/nnurosign.jpg" alt="login" class="login-card-img">
           </div>
-          <div class="col-md-7">
+          <div class="col-md-7 style= padding-right:70%" >
             <div class="card-body">
               <div class="brand-wrapper" >
                 <img style="height: 65px; width: 20%" src="admin/assets/images/log.png" alt="logo" class="logo">
@@ -60,22 +60,37 @@
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                  </div>
+
+                  <div class="form-group row">
+                            <div class="col-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label style="width:150px;" class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                 
                   <div class= "text-left">
-                  <div class="form-group row mb-0">
+                  <div class="form-group row mb-4">
                             <div class="col-md-8 offset-md-0">
                                 <button type="submit" class="btn btn-block login-btn mb-3 ">
                                     {{ __('Login') }}
                                 </button>
                         </div>
-                    
-                                @if (Route::has('password.request'))
+                        @if (Route::has('password.request'))
                                 <div style="margin-left:16px;">
                                     <a class="forgot-password-link"  href="{{ route('password.request') }}" >
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 </div>
                                 @endif
+                        </div>
+                    
+                                
 
                             
                         </div>
