664a9a3071f7933f527f823474530eb8aab0d4cd
Avalon <hopegag@gmail.com>
2020-06-05 00:24:05 +0000

    Modified Login Header

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,7 +30,7 @@
                   <div class="form-group">
                     <label for="email" class="">{{ __('Location') }}</label>
                     @if (!is_null($locations))
-                    <select class="form-control" name="location" id="">
+                    <select class="form-control" name="location" required id="">
                         <option value="">Select</option>
                         @foreach ($locations as $location)
                             <option value="{{$location->id}}">{{$location->name}}</option>
@@ -44,7 +44,7 @@
                     @enderror
                   </div>
                   <div class="form-group">
-                    <label for="email" class="">{{ __('Company Name') }}</label>
+                    <label for="email" class="">{{ __('Company Name | E-mail') }}</label>
                     <input type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                     @error('email')
                         <span class="invalid-feedback" role="alert">
@@ -56,25 +56,25 @@
                     <label for="password" class="sr-only">{{ __('Password') }}</label>
                     <input type="password" id="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                     @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                   </div>
                   <div class= "text-left">
                   <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-0">
-                                <button type="submit" class="btn btn-block login-btn mb-3 ">
-                                    {{ __('Login') }}
-                                </button>
+                        <div class="col-md-8 offset-md-0">
+                          <button type="submit" class="btn btn-block login-btn mb-3 ">
+                              {{ __('Login') }}
+                          </button>
                         </div>
-                                @if (Route::has('password.request'))
-                                <div style="margin-left:16px;">
-                                    <a class="forgot-password-link"  href="{{ route('password.request') }}" >
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                </div>
-                                @endif
+                        @if (Route::has('password.request'))
+                        <div style="margin-left:16px;">
+                            <a class="forgot-password-link"  href="{{ route('password.request') }}" >
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        </div>
+                        @endif
 
                             
                         </div>
