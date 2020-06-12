7c42953da6c95833b1e06e5e411422d65cb34353
Avalon <hopegag@gmail.com>
2020-06-12 02:50:37 +0000

    Refactured Purchase or Product headers

--- resources/views/admin/pages/retailers/purchase_order-list.blade.php
+++ resources/views/admin/pages/retailers/purchase_order-list.blade.php
@@ -45,7 +45,6 @@
                         <table id="datatable" class="table table-bordered dt-responsive nowrap dataTable no-footer" style="border-collapse: collapse; border-spacing: 0px; width: 100%;" role="grid" aria-describedby="datatable_info">
                             <thead>
                                 <tr role="row">
-                                    <th class="sorting_asc" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Product Name: activate to sort column descending" style="width: 150px;">Product Name</th>
                                     <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Category: activate to sort column ascending" style="width: 170px;">Description</th>
                                     <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Avai.Color: activate to sort column ascending" style="width: 130px;">Manufacturer</th>
                                     <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Price: activate to sort column ascending" style="width: 69px;">Price</th>
@@ -67,9 +66,8 @@
                             @if (!Cart::isEmpty())
                                 @foreach (Cart::getContent() as $item)
                                     <tr>
-                                        <td>{{$item->name}}</td>
-                                        <td>{{$item->associatedModel->manufacturers->name}}</td>
-                                        <td>{{$item->associatedModel->productDescription()}} </td>
+                                        <td>{{$item->associatedModel->productDesc()}} </td>
+                                        <td>{{$item->associatedModel->manufacturer->name}}</td>
                                         <td> {{$item->price}}</td>
                                         <form action="{{route('cart.update',$item->id )}}" method="POST">
                                             @method('PUT')
