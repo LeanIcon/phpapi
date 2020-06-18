0bc731462d7c780b41d60733c114bd4d71d4d65e
Avalon <hopegag@gmail.com>
2020-06-18 11:54:19 +0000

    Modified route after signup

--- routes/web.php
+++ routes/web.php
@@ -105,7 +105,7 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Web'], function() {
 
 Route::get('verify', 'Web\VerifyPinPageController@loadpage')->name('loadpin');
 Route::post('verify', 'Web\ConfirmPinController');
-Route::get('verify', 'Web\VerifyPinPageController@sendVerify');
+// Route::get('verify', 'Web\VerifyPinPageController@sendVerify');
 
 
 Auth::routes(['register' => false]);
