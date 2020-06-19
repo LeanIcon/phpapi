67cabf35557ea586efbb06f0cbf8a22bf420a616
Avalon <hopegag@gmail.com>
2020-06-19 01:21:20 +0000

    Modified Wholesaler Product Controller to load using relations

--- resources/views/admin/pages/register/wholesaler.blade.php
+++ resources/views/admin/pages/register/wholesaler.blade.php
@@ -147,18 +147,6 @@
 <!-- jQuery  -->
 <script src="{{url('admin/assets/js/jquery.min.js')}}"></script>
 <script src="{{url('admin/assets/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{url('admin/assets/js/metisMenu.min.js')}}"></script>
-<script src="{{url('admin/assets/js/waves.min.js')}}"></script>
-<script src="{{url('admin/assets/js/jquery.slimscroll.min.js')}}"></script>
-<script src="{{url('admin/assets/plugins/moment/moment.js')}}"></script>
-<script src="{{url('admin/assets/plugins/apexcharts/apexcharts.min.js')}}"></script>
-<script src="{{url('admin/assets/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js')}}"></script>
-<script src="{{url('admin/assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
-<script src="{{url('admin/assets/pages/jquery.eco_dashboard.init.js')}}"></script>
-{{--  <script src="{{url('admin/plugins/datatables/jquery.dataTables.min.js')}}"></script>
-<script src="{{url('admin/plugins/datatables/dataTables.bootstrap4.min.js')}}"></script>
-<script src="{{url('admin/pages/jquery.crm_leads.init.js')}}"></script>  --}}
-
 <!-- App js -->
 <script src="{{url('admin/assets/js/app.js')}}"></script>
 
