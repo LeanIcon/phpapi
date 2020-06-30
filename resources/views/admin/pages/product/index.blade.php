4f3efc783601d9d6fd9317d1fdabbdabad882da2
programmeroscar <programmeroscar@gmail.com>
2020-06-30 16:13:40 +0000

    edited admin products table headers and also edited productDesc function

--- resources/views/admin/pages/product/index.blade.php
+++ resources/views/admin/pages/product/index.blade.php
@@ -33,6 +33,7 @@
                     <table id="datatable" class="table">
                         <thead class="thead-light">
                         <tr>
+                            <th>Product Name</th>
                             <th>Product Description</th>
                            {{-- <th>Product Description(Active Ingredient , Strength)</th> --}}
                             <th>Manufacturer</th>
@@ -46,6 +47,7 @@
                         @if ($products->isNotEmpty())
                             @foreach ($products as $product)
                                 <tr>
+                                    <td>{{ $product->name }}</td>
                                 <td>{{$product->productDesc() ?? 'NA'}}</td>
                                     <td>{{$product->manufacturer->name ?? $product->manufacturer_slug}}</td> 
                                     <td> {{$product->packet_size}}</td>
