2eb093ac87d6636e17e09e9228248b5fb626d72c
gaby96 <gabrieladdo64@gmail.com>
2020-07-08 09:25:32 +0000

    Added Datatables to sort prices in price comparison page

--- resources/views/admin/pages/retailers/search.blade.php
+++ resources/views/admin/pages/retailers/search.blade.php
@@ -1,5 +1,4 @@
 @extends('admin.index')
-
 @section('content')
 @include('admin.layouts.components.breadcrumbs', ['pageTitle' => $pageTitle ?? ''])
 <div class="container">
@@ -10,7 +9,6 @@
               <button class="btn aqua-gradient btn-rounded btn-sm my-0 waves-effect waves-light" type="submit">Search</button>
             </form>
             <br>
-
             <div class="row">
                         <div class="col-sm-12">
                             <table id="datatable" class="table table-bordered dt-responsive nowrap dataTable no-footer" style="border-collapse: collapse; border-spacing: 0px; width: 100%;" role="grid" aria-describedby="datatable_info">
@@ -19,44 +17,34 @@
                                         <th class="sorting_asc" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Product Name: activate to sort column descending" style="width: 50px;">Wholesaler Name </th>
                                         <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Price: activate to sort column ascending" style="width: 69px;">Product Name</th>
                                         <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Price: activate to sort column ascending" style="width: 69px;">Active Description</th>
-                                        
                                         <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Price: activate to sort column ascending" style="width: 69px;">Manufacturer</th>
                                         <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Price: activate to sort column ascending" style="width: 69px;">Pack Size</th>
                                         <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Price: activate to sort column ascending" style="width: 69px;">Unit Price</th>
                                     </tr>
-                                </thead>
-    
-            @if(isset($sor))
+                                </thead> <tbody>
+                                @if(isset($sor))
                 @if ($sor-> isEmpty())
                     <h2>Sorry, no products were found with the name <b>"{{ $searchterm }}"</b>.</h2>
                 @else
                     <h2> We have found {{ $sor->count() }} products with the name  <b>"{{ $searchterm }}"</b></h2>
                     <hr />
-                    
                     @foreach($sor->groupByType() as $type => $modelSearchResults)
                     <!-- <h2>{{ ucwords($type) }}</h2> -->
-    
                     @foreach($modelSearchResults as $searchResult)
-
-                    <tbody>
-                    <tr role="row">
+                    <tr role="row" class="odd">
                         @foreach($wholesalers as $wholesaler)
-
                             @if ($wholesaler->id == $searchResult->searchable->wholesaler_id)
-
                                 <td><a href="{{route('retailer.wholesaler.show', $searchResult->searchable->wholesaler_id)}}"> {{$wholesaler->name}} </a> </td>
                             @endif
                             <!-- @if($searchResult->searchable->wholesaler_id == $wholesaler->id )
                                 <td> {{$wholesalers[0]['name']}}</td>
                             @endif -->
                         @endforeach
-                    
                             <td>    <a href="{{route('retailer.wholesaler.show', $searchResult->searchable->wholesaler_id)}}">{{ $searchResult->searchable->product_name }}</a> </td>
                             <td>   <a href="{{ $searchResult->url }}">{{ $searchResult->searchable->active_ingredient }} {{ $searchResult->searchable->strength }} {{ $searchResult->searchable->dosage_form }}</a> </td>
                             <td>   <a href="{{ $searchResult->url }}">{{ $searchResult->searchable->manufacturer }}</a>  </td>
                             <td>   <a href="{{ $searchResult->url }}">{{ $searchResult->searchable->packet_size }}</a>  </td>
-                            <td>   <a href="{{ $searchResult->url }}">{{ $searchResult->searchable->formattedprice() }}</a>  </td>
-                        
+                            <td>   <a href="{{ $searchResult->url }}">{{ $searchResult->searchable->formattedPrice() }}</a>  </td>
                     </tr>
                     @endforeach
                     @endforeach
@@ -69,9 +57,7 @@
         </div>
     </div>
 </div>
-
 @endsection
-
 @section('page-js') 
 <script>
     $(document).ready(function() {
