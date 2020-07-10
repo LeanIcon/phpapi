050bcc1ec0397ef3842acdceb921ac3caabee64c
Avalon <hopegag@gmail.com>
2020-07-10 01:49:00 +0000

    Removed Blade Files

--- resources/views/admin/pages/retailers/retailer_shortagelist.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('admin.index')
-
-@section('content')
-@include('admin.layouts.components.breadcrumbs', ['pageTitle' => $pageTitle ?? '']) 
-    @include('admin.pages.retailers.products.list')
-@endsection
