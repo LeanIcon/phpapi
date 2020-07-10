8be90042a88750f9b2cb9dcca3bfcd36971fcc86
Avalon <hopegag@gmail.com>
2020-07-10 01:10:41 +0000

    updated content

--- resources/views/admin/layouts/notifications/wholesaler_notify.blade.php
+++ resources/views/admin/layouts/notifications/wholesaler_notify.blade.php
@@ -4,11 +4,13 @@
     <!-- item-->
         @if ($wholesalerpo->count() > 0)
         @foreach ($wholesalerpo as $po)
+        @if ($po->status ==  'pending')
         <a href="{{route('retailer.purchaselist')}}" class="dropdown-item notify-item active">
             <div class="notify-icon bg-success"><i class="mdi mdi-cart-outline"></i></div>
             <p class="notify-details"> Raised Purchase Order
             <small class="text-muted">{{$po->status}}</small></p>
         </a>
+        @endif
         @endforeach
         @endif
 
