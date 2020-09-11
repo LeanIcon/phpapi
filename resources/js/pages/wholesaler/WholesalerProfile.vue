08455faf172633d20606ea064b293f5681d1d97e
Equinox <hopegag@gmail.com>
2020-09-11 06:22:08 +0100

    remove product tab from profile page

--- resources/js/pages/wholesaler/WholesalerProfile.vue
+++ resources/js/pages/wholesaler/WholesalerProfile.vue
@@ -1,145 +1,114 @@
 <template>
-  <div>
+<div>
     <div class="row">
-      <div class="col-lg-8">
-        <div class="card">
-          <div class="row no-gutters align-items-center">
-            <div class="col-md-4">
-              <img
-                class="card-img img-fluid"
-                :src="'/assets/images/small/img-2.jpg'"
-                alt="Card image"
-              />
+        <div class="col-lg-8">
+            <div class="card">
+                <div class="row no-gutters align-items-center">
+                    <div class="col-md-4">
+                        <img class="card-img img-fluid" :src="'/assets/images/small/img-2.jpg'" alt="Card image" />
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Identity: {{authUser.name}}</h5>
+                            <p class="card-text">Location: {{authUser.details.location == null ? 'Not Availabe' : authUser.details.location}}</p>
+                            <p class="card-text">
+                                Status:
+                                <small class="text-muted">Active</small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">Identity: {{authUser.name}}</h5>
-                <p
-                  class="card-text"
-                >Location: {{authUser.details.location == null ? 'Not Availabe' : authUser.details.location}}</p>
-                <p class="card-text">
-                  Status:
-                  <small class="text-muted">Active</small>
-                </p>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="card">
-          <div class="row no-gutters align-items-center">
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">Contact Details</h5>
-                <p
-                  class="card-text"
-                >Contact Person: {{authUser.details.contact_person == null ? 'Not Availabe' : authUser.details.contact_person}}</p>
-                <p class="card-text">
-                  Status:
-                  <small class="text-muted">Active</small>
-                </p>
-              </div>
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="row no-gutters align-items-center">
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Contact Details</h5>
+                            <p class="card-text">Contact Person: {{authUser.details.contact_person == null ? 'Not Availabe' : authUser.details.contact_person}}</p>
+                            <p class="card-text">
+                                Status:
+                                <small class="text-muted">Active</small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-      </div>
-      <!-- End of Top Card -->
-      <div class="col-lg-12">
+    </div>
+    <!-- End of Top Card -->
+    <div class="col-lg-12">
 
         <div class="card">
-          <div class="card-body">
-            <div class="mt-4">
-              <h5 class="font-size-14 mb-3">Profile / Settings and Configurations</h5>
-              <div class="product-desc">
-                <ul class="nav nav-tabs nav-tabs-custom" role="tablist">
-                  <li class="nav-item">
-                    <a
-                      class="nav-link"
-                      id="desc-tab"
-                      data-toggle="tab"
-                      href="#profile"
-                      role="tab"
-                      aria-selected="false"
-                    >Profile</a>
-                  </li>
-                  <li class="nav-item">
-                    <a
-                      class="nav-link active"
-                      id="specifi-tab"
-                      data-toggle="tab"
-                      href="#specifi"
-                      role="tab"
-                      aria-selected="true"
-                    >System / Application Settings</a>
-                  </li>
-                  <li class="nav-item" v-if="isWholesaler" >
-                    <a
-                      class="nav-link"
-                      id="product-tab"
-                      data-toggle="tab"
-                      href="#product"
-                      role="tab"
-                      aria-selected="true"
-                    >Products</a>
-                  </li>
-                </ul>
-                <div class="tab-content border border-top-0 p-4">
-                  <div class="tab-pane fade" id="profile" role="tabpanel">
-                    <div>
-                      <detail-form :userdetails="authUser.details"></detail-form>
+            <div class="card-body">
+                <div class="mt-4">
+                    <h5 class="font-size-14 mb-3">Profile / Settings and Configurations</h5>
+                    <div class="product-desc">
+                        <ul class="nav nav-tabs nav-tabs-custom" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link" id="desc-tab" data-toggle="tab" href="#profile" role="tab" aria-selected="false">Profile</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" id="specifi-tab" data-toggle="tab" href="#specifi" role="tab" aria-selected="true">System / Application Settings</a>
+                            </li>
+
+                        </ul>
+                        <div class="tab-content border border-top-0 p-4">
+                            <div class="tab-pane fade" id="profile" role="tabpanel">
+                                <div>
+                                    <detail-form :userdetails="authUser.details"></detail-form>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade active show" id="specifi" role="tabpanel">
+                                <app-settings />
+                            </div>
+
+                        </div>
                     </div>
-                  </div>
-                  <div class="tab-pane fade active show" id="specifi" role="tabpanel">
-                    <app-settings />
-                  </div>
-                  <div class="tab-pane fade" id="product" role="tabpanel">
-                    <h4>Loading Products</h4>
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-  
-  </div>
+    </div>
+
+</div>
 </template>
 
 <script>
-import { UserTypes } from '../../_helpers/role'
+import {
+    UserTypes
+} from '../../_helpers/role'
 import DetailsFormVue from "../../components/DetailsForm.vue";
 import ApplicationSettingsVue from "../../components/ApplicationSettings.vue";
 export default {
-  components: {
-    detailForm: DetailsFormVue,
-    appSettings: ApplicationSettingsVue,
-  },
-  data() {
-    return {
-      notifications: false,
-      product_updates: false,
-      news_infos: false,
-      shortage_lists: false,
-      pos_demos: false,
-      product_expiry: false,
-    };
-  },
-  computed: {
-    authUser() {
-      return this.$store.getters["account/userData"];
+    components: {
+        detailForm: DetailsFormVue,
+        appSettings: ApplicationSettingsVue,
     },
-    isWholesaler() {
-      return this.$store.getters["account/userType"] == UserTypes.wholesaler;
+    data() {
+        return {
+            notifications: false,
+            product_updates: false,
+            news_infos: false,
+            shortage_lists: false,
+            pos_demos: false,
+            product_expiry: false,
+        };
     },
-    isRetailer() {
-      return this.$store.getters["account/userType"] == UserTypes.retailer;
+    computed: {
+        authUser() {
+            return this.$store.getters["account/userData"];
+        },
+        isWholesaler() {
+            return this.$store.getters["account/userType"] == UserTypes.wholesaler;
+        },
+        isRetailer() {
+            return this.$store.getters["account/userType"] == UserTypes.retailer;
+        },
     },
-  },
-  mounted() {
-    console.log(this.authUser);
-  },
+    mounted() {
+        console.log(this.authUser);
+    },
 };
 </script>
 
