9c6ecb074d37352a896f41abfe61dae7dde7786d
gaby96 <gabrieladdo64@gmail.com>
2020-09-23 01:06:11 +0000

    Refactored Retailer page with addition of carousel for advert

---
+++ resources/js/components/MainAdvert.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <carousel :autoplay="true" :nav="true" :items=2>
+              <img src="assets/images/nnlogo.png">
+              <img src="assets/images/AD.jpg">
+              <img src="assets/images/AD 2.jpg">
+              <img src="assets/images/AD3.jpg">
+              
+          </carousel>
+  </div>
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel2'
+export default {
+    components: {
+        carousel
+    }
+
+}
+</script>
+
+<style>
+
+</style>
