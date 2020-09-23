d454826b33356dcd94415862dd01b599f4d0e1df
gaby96 <gabrieladdo64@gmail.com>
2020-09-23 11:30:20 +0000

    Refactored display of adverts in retailer dashboard

--- resources/js/components/MainAdvert.vue
+++ resources/js/components/MainAdvert.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
-      <carousel :autoplay="true" :nav="true" :items=2>
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="assets/images/nnlogo.png" style="width: 70%; height: 220px;">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/images/AD.jpg" style="width: 70%; height: 220px;">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/images/AD 2.jpg" style="width: 70%; height: 220px;">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+      <!-- <carousel :autoplay="true" :nav="false" :items=2>
               <img src="assets/images/nnlogo.png">
               <img src="assets/images/AD.jpg">
               <img src="assets/images/AD 2.jpg">
               <img src="assets/images/AD3.jpg">
               
-          </carousel>
+          </carousel> -->
   </div>
 </template>
 
