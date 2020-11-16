2b8a1078f7778f27c67b1733d4b1c6b163cf91fb
gaby96 <gabrieladdo64@gmail.com>
2020-11-16 21:32:42 +0000

    refactored placeholder

--- resources/js/pages/RegisterPage.vue
+++ resources/js/pages/RegisterPage.vue
@@ -67,7 +67,7 @@
                                                     </div>
                                                     <div class="form-group  mb-2 col-lg-6">
                                                         <label for="username">Email</label>
-                                                        <input type="email" v-model="user.email" class="form-control" id="username" placeholder="email">
+                                                        <input type="email" v-model="user.email" class="form-control" id="username" placeholder="Email Address">
                                                     </div>
                                                 </div>
                                                 <div class="row">
